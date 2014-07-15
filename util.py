d34458ff66625842dfa8a63fd088b3ca12b7497c
Timidger <APragmaticPlace@gmail.com>
2014-07-15 01:03:13 -0500

    Updated util.py to Python3 (Just parens around the print statements)

--- util.py
+++ util.py
@@ -7,19 +7,19 @@ def formatted(*args):
 
 
 def log(*args):
-    print apply(formatted,args)
+    print(apply(formatted,args))
 
 def fail(*args):
-    print '\033[91m'+apply(formatted,args)+'\033[0m'
+    print('\033[91m'+apply(formatted,args)+'\033[0m')
 
 def warn(*args):
-    print '\033[93m'+apply(formatted,args)+'\033[0m'
+    print('\033[93m'+apply(formatted,args)+'\033[0m')
 
 def success(*args):
-    print '\033[92m'+apply(formatted,args)+'\033[0m'
+    print('\033[92m'+apply(formatted,args)+'\033[0m')
     
 def special(*args):
-    print '\033[95m'+apply(formatted,args)+'\033[0m'
+    print('\033[95m'+apply(formatted,args)+'\033[0m')
     
 def bluelog(*args):
-    print '\033[94m'+apply(formatted,args)+'\033[0m'
+    print('\033[94m'+apply(formatted,args)+'\033[0m')
